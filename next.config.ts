925ce214c03d0bfbff626fe48f7dd81c67f23842
Soham Banerjee <soham@renderbit.com>
2026-01-30 12:21:42 +0530

    Update next.config.ts with new configuration options
    Added configuration options for output, trailing slash, and distribution directory.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: false,
+
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+
   images: {
     remotePatterns: [
       {
